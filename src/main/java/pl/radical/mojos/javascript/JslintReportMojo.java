889d57d2bc73271ea157c5b767997abb02f4bafb
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2010-05-08 13:46:30 +0000

    git-svn-id: http://svn.radical.com.pl/maven2/maven-javascript-plugin/trunk@2480 01953d5f-b815-0410-ab08-b360c6b68d4c

---
+++ src/main/java/pl/radical/mojos/javascript/JslintReportMojo.java
@@ -0,0 +1,46 @@
+package pl.radical.mojos.javascript;
+
+import java.util.Locale;
+
+import org.apache.maven.doxia.siterenderer.Renderer;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.reporting.AbstractMavenReport;
+import org.apache.maven.reporting.MavenReportException;
+
+public class JslintReportMojo extends AbstractMavenReport {
+
+	protected void executeReport(final Locale locale) throws MavenReportException {
+		// TODO Auto-generated method stub
+	}
+
+	protected String getOutputDirectory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	protected MavenProject getProject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	protected Renderer getSiteRenderer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDescription(final Locale locale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getName(final Locale locale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getOutputName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/pl/radical/mojos/javascript/MyMojo.java
@@ -0,0 +1,17 @@
+package pl.radical.mojos.javascript;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Goal which touches a timestamp file.
+ * 
+ * @goal jslint-report
+ * @phase process-sources
+ */
+public class MyMojo extends AbstractMojo {
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+	}
+}
